d1c44c1ded9c54c9821626f72dbcdcfc32adca83
Leiziane Trevisan Dardin <leiziane.trevisan@yahoo.com.br>
2026-03-26 14:11:24 -0600

    Adding student Number

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '60px', fontFamily: 'Arial' }}>
       <h1>Group 1</h1>
-      <p>Leiziane Trevisan Dardin</p>
+      <p>Leiziane Trevisan Dardin 459656</p>
       <p>Ingrid Tufani Savioli</p>
       <p>Sabrina Johanna Zimmer</p>
       <p>Patricia Diniz Magalhaes</p>
